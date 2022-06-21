ebd906f1371ef8f43cd323e73bfa30d027bce46d
Gustavo Perbone <guperbone@yahoo.com.br>
2022-06-21 19:02:52 -0300

    workin on it...

--- projeto2.py
+++ projeto2.py
@@ -19,17 +19,7 @@ def obter_dados() -> list:
 
     return dados
 
-def apendar_dados(novo_dado: list) -> None:
-    '''
-    Função para gravar novos dados no programa.
-    PARAMETROS: lista
-    RETORNO: N/A
-    '''
-    with open(('dados.csv'), 'a') as arq:
-        escritor = csv.writer(arq, delimiter=",", lineterminator="\n")
-        escritor.writerow(novo_dado)
-
-def reescrever_dados(dados: list) -> None:
+def salvar_dados(dados: list) -> None:
     '''
     Função para reescrever dados no programa.
     PARAMETROS: lista
@@ -43,7 +33,7 @@ def pega_opcao() -> str:
     '''
     Função para obter a opção desejada pelo user.
     PARAMETROS: N/A
-    RETORNO: inteiro, já validado, representando uma das opções disponíveis
+    RETORNO: string, já validado, representando uma das opções disponíveis
     '''
     print('''
 ::: LISTA DE OPÇÕES :::
@@ -59,7 +49,7 @@ def pega_opcao() -> str:
     while not(opt.isdigit()):
         opt = input("Digite apenas números! Digite a opção desejada: ")
 
-    return int(opt) if opt in "12340" else pega_opcao()
+    return opt if int(opt) in [1, 2, 3, 4, 0] else pega_opcao()
 
 def valida_nome() -> str:
     '''
@@ -75,14 +65,14 @@ def valida_nome() -> str:
         print("O nome deve conter apenas letras e espaços. Tente novamente.")
         return valida_nome()
 
-def valida_email() -> str:
+def valida_email(dados:list) -> str:
     '''
     Função para obter o email do músico, validado segundo regras (letras, underscore (_), ponto (.), dígitos numéricos e, obrigatoriamente, exatamente 1 arroba (@)).
     PARAMETROS: N/A
     RETORNO: string, já validado e formatado, representando o email do músico
     '''
     email = input("Digite o e-mail do músico: ").strip().lower()
-    eh_repetido = any([True if email == entrada[1] else False for entrada in obter_dados()])
+    eh_repetido = any([True if email == entrada[1] else False for entrada in dados])
     email_helper = email.replace("_", "").replace(".", "").replace("@", "")
     if email_helper.isalnum() and email.count("@") == 1 and eh_repetido == False:
         return email
@@ -93,17 +83,33 @@ def valida_email() -> str:
         print("O email deve conter apenas letras, underline, pontos e exatamente um @ apenas letras e espaços. Tente novamente.")
         return valida_email()
 
-def cadastrar_musico() -> None:
+def valida_genero() -> list:
+    generos = input("Digite os gêneros musicais de interesse do músico (se for mais de 1, separar com vírgulas): ").strip()
+    if generos == "":
+        print("Digite pelo menos um!")
+        return valida_genero()
+    else: 
+        return [genero.strip().lower() for genero in generos.split(",")]
+    
+def valida_instrumento() -> list:
+    instrumentos = input("Digite os instrumentos que o músico toca (se for mais de 1, separar com vírgulas): ").strip()
+    if instrumentos == "":
+        print("Digite pelo menos um!")
+        return valida_instrumento()
+    else: 
+        return [instrumento.strip().lower() for instrumento in instrumentos.split(",")]
+
+def cadastrar_musico(dados:list) -> list:
     nome = valida_nome()
-    email = valida_email()
-    generos = input("Digite os gêneros musicais de interesse do músico (se for mais de 1, separar com vírgulas): ")
-    lista_generos = [genero.strip().lower() for genero in generos.split(",")]
-    instrumentos = input("Digite os instrumentos que o músico toca (se for mais de 1, separar com vírgulas): ")
-    lista_instrumentos = [instrumento.strip().lower() for instrumento in instrumentos.split(",")]
+    email = valida_email(dados)
+    lista_generos = valida_genero()
+    lista_instrumentos = valida_instrumento()
 
     lista_dados = [nome, email, lista_generos, lista_instrumentos]
 
-    apendar_dados(lista_dados)
+    dados.append(lista_dados)
+
+    return dados
 
 def busca_de_dados(dados: list, dados_para_busca: list, modo_de_busca: int) -> list:
     indices_nome, indices_email, indices_genero, indices_instrumento= [], [], [], []
@@ -141,7 +147,7 @@ def imprimir_resultados_busca(resultados:list) -> None:
     else:
         print("Não há resultados que correpondam à sua busca!")
 
-def buscar_musicos() -> None:
+def buscar_musicos(dados:list) -> list:
     nome = input("Digite um nome para busca, ou aperte enter para continuar: ").strip().title()
     email = input("Digite um e-mail para busca, ou aperte enter para continuar: ").strip().lower()
     genero = input("Digite um gênero musical, ou aperte enter para continuar: ").strip().lower()
@@ -150,86 +156,118 @@ def buscar_musicos() -> None:
 
     try:
         modo_de_busca = int(modo)
-        dados = obter_dados()
         dados_para_busca = [nome, email, genero, instrumento]
         if modo_de_busca == 1 or modo_de_busca == 2:
             resultados = busca_de_dados(dados, dados_para_busca, modo_de_busca)
             imprimir_resultados_busca(resultados)
         else:
             raise Exception
-    except (ValueError, Exception):
+    except (ValueError, Exception) as e:
         print("Parâmetros incorretos. Tente novamente!")
+    finally:
+        return dados
 
-def substituicao_de_dados(user_encontrado:list) -> None:
-    dados = obter_dados()
-    dados = [dado for dado in dados if dado != user_encontrado[0]]
-    generos = user_encontrado[0][2]
-    instrumentos = user_encontrado[0][3]
+def substituicao_de_dados(dados: list, user_encontrado:list) -> None:
 
-    print(f"User: {user_encontrado[0][0]} | Gênero(s) de interesse cadastrados: {', '.join(generos)}")
-    for i in range(len(generos)):
-        input_user = input(f"{generos[i].upper()} : Aperte ENTER para MANTER o gênero ou digite QUALQUER COISA para EXCLUIR: ").strip().lower()
-        generos[i] = generos[i] if input_user == "" else ""
+    print(f"User: {dados[user_encontrado][0]} | Gênero(s) de interesse cadastrados: {', '.join(dados[user_encontrado][2])}")
+    for i in range(len(dados[user_encontrado][2])):
+        input_user = input(f"{dados[user_encontrado][2][i].upper()} : Aperte ENTER para MANTER o gênero ou digite QUALQUER COISA para EXCLUIR: ").strip().lower()
+        dados[user_encontrado][2][i] = dados[user_encontrado][2][i] if input_user == "" else ""
     input_user = input(f"Caso deseje adicionar mais gêneros, digite-os separados por vírgula: ").strip().lower()
     input_user = [item.strip() for item in input_user.split(",")]
-    generos.extend(input_user)
-    while '' in generos:
-        generos.remove('')
+    dados[user_encontrado][2].extend(input_user)
+    while '' in dados[user_encontrado][2]:
+        dados[user_encontrado][2].remove('')
 
-    print(f"User: {user_encontrado[0][0]} | Instrumento(s) cadastrados: {', '.join(instrumentos)}")
-    for i in range(len(instrumentos)):
-        input_user = input(f"{instrumentos[i].upper()} : Aperte ENTER para MANTER o instrumento ou digite QUALQUER COISA para EXCLUIR: ").strip().lower()
-        instrumentos[i] = instrumentos[i] if input_user == "" else ""
+    print(f"User: {dados[user_encontrado][0]} | Instrumento(s) de interesse cadastrados: {', '.join(dados[user_encontrado][3])}")
+    for i in range(len(dados[user_encontrado][3])):
+        input_user = input(f"{dados[user_encontrado][3][i].upper()} : Aperte ENTER para MANTER o gênero ou digite QUALQUER COISA para EXCLUIR: ").strip().lower()
+        dados[user_encontrado][3][i] = dados[user_encontrado][3][i] if input_user == "" else ""
     input_user = input(f"Caso deseje adicionar mais instrumentos, digite-os separados por vírgula: ").strip().lower()
     input_user = [item.strip() for item in input_user.split(",")]
-    instrumentos.extend(input_user)  
-    while '' in instrumentos:
-        instrumentos.remove('')  
+    dados[user_encontrado][3].extend(input_user)
+    while '' in dados[user_encontrado][3]:
+        dados[user_encontrado][3].remove('')
 
-    dados.append([user_encontrado[0][0], user_encontrado[0][1], generos, instrumentos])
-    print(dados)
     return dados
 
-def modificar_musico() -> None:
+def modificar_musico(dados:list) -> None:
     email = input("Digite o email do usuário para modificar: ").strip().lower()
     email_helper = email.replace("_", "").replace(".", "").replace("@", "")
     if email_helper.isalnum() and email.count("@") == 1:
-        user_encontrado = busca_de_dados(obter_dados(), ['', email, '', ''], 1)
+        user_encontrado = [index for index in range(len(dados)) if dados[index][1] == email][0]
         if user_encontrado != []:
-            dados_substituidos = substituicao_de_dados(user_encontrado)
-            reescrever_dados(dados_substituidos)
-            print(f"Usuário {dados_substituidos[-1][0]} atualizado com sucesso!")
+            dados_substituidos = substituicao_de_dados(dados, user_encontrado)
+            print(f"Usuário {dados_substituidos[user_encontrado][0]} atualizado com sucesso!")
         else:
            print(f"Não há usuários com o e-mail digitado. Tente novamente!") 
+           dados_substituidos = dados
     else: 
         print("O e-mail digitado é inválido. Tente novamente.")
+        dados_substituidos = dados
 
-def menu() -> None:
-    ativo = 1
+    return dados
 
-    while ativo == 1:
+def executa_combinacoes(musicos_por_instrumento:list, instrumentos:list) -> list:
+    combinacoes = []
+
+    while len(musicos_por_instrumento) > 1:
+        for musico in musicos_por_instrumento[0]:
+            for outro_musico in musicos_por_instrumento[1]:
+                combinacoes.append([musico, outro_musico])
+            
+        restante = [combinacoes] + musicos_por_instrumento[2:]
+
+    return combinacoes(restante) if len(musicos_por_instrumento) > 1 else musicos_por_instrumento
+
+def limpa_combinacoes(resultado: list) -> list:
+    lista_emails = [musico[1] for musico in resultado for musicos in resultado]
+
+    return [banda for index, banda in enumerate(resultado) if len(set(lista_emails[index])) == len(banda)]
+
+def imprime_combinacoes(resultado: list, instrumentos: list) -> None:
+    for i in range(len(resultado)):
+        print(f"Banda {i+1}:")
+        for item in resultado[i]:
+            print(f"{instrumentos[i]}.upper() : {resultado[i][0]} | {resultado[i][1]} | {', '.join(resultado[i][2])} | {', '.join(resultado[i][3])}")
+
+def montar_banda(dados: list):
+    genero_busca = input("Digite o gênero desejado para sua banda: ").strip().lower()
+    quantidade = input("Digite a quantidade de músicos para sua banda: ").strip()
+    while not(quantidade.isdigit()):
+        quantidade = input("Digite a quantidade de músicos para sua banda: ").strip()
+    quantidade = int(quantidade)
+    instrumentos = []
+    for i in range(quantidade):
+        instrumentos.append(input(f"Digite o {i+1}o instrumento para sua banda: ").strip())
+    #filtra musicos do genero por instrumentos
+
+    lista_por_instrumentos = []
+    for i in range(len(instrumentos)):
+        for musico in dados:
+            if instrumentos[i] in musico[3] and genero_busca in musico[2]:
+                lista_por_instrumentos.append(musico)
+
+    resultado = executa_combinacoes(lista_por_instrumentos, instrumentos)
+    resultado = limpa_combinacoes(resultado)
+    imprime_combinacoes(resultado, instrumentos)
+
+    return dados
+
+def menu(dados:list) -> None:
+    opcoes = {
+        "1": cadastrar_musico,
+        "2": buscar_musicos,
+        "3": modificar_musico,
+        "4": montar_banda
+    }
+    opt = pega_opcao()
+    while opt != "0":
+        dados = opcoes[opt](dados)
         opt = pega_opcao()
+    else:
+        salvar_dados(dados)
+        print("Até mais!")
 
-        if opt == 1:
-            cadastrar_musico()
-            #falta validar se nao eh email repetid
-            ativo = 1
-
-        elif opt == 2:
-            buscar_musicos()
-            ativo = 1
-
-        elif opt == 3:
-            modificar_musico()
-            ativo = 1
-
-        elif opt == 0:
-            print("Até mais!")
-            ativo = 0
-
-        else: 
-            print("Opção inválida. Tente novamente.")
-            ativo = 1
-
-
-menu()
+dados = obter_dados()
+menu(dados)
